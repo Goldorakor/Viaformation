47fb5e159d0529d7061750016ef1e1ef280c969e
Michaël HEID <michael.heid@viaformation.e-cdp.com>
2023-09-19 11:05:20 +0200

    base exercice

--- ProjetAlex/Program.cs
+++ ProjetAlex/Program.cs
@@ -1,20 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using System;
 
-Console.WriteLine("Hello, World! How are you ?");
-int A;
-int B;
-int C;
 
-A = 3;
-B = 12;
-
-C = B;
-B = A;
-A = C;
-
-Console.WriteLine("La valeur de A est ");
-Console.WriteLine(A);
-Console.WriteLine("La valeur de B est ");
-Console.WriteLine(B);
 
