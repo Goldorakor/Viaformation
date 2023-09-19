9a7612354732ffcda195a7790d1249faaed187e5
Michaël HEID <michael.heid@viaformation.e-cdp.com>
2023-09-19 10:01:13 +0200

    commit initial

---
+++ ProjetAlex/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+Console.WriteLine("Hello, World!");
+int A;
+int B;
+int C;
+
+A = 3;
+B = 12;
+
+C = B;
+B = A;
+A = C;
+
+Console.WriteLine("La valeur de A est ");
+Console.WriteLine(A);
+Console.WriteLine("La valeur de B est ");
+Console.WriteLine(B);
+
