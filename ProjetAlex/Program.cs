4f7ae6372e9adea691983506613d0b0e6a91dad8
MichaëlHEID <MichalHEID@TRD013-SBG2FR>
2023-09-26 13:56:12 +0200

    exo 24

--- ProjetAlex/Program.cs
+++ ProjetAlex/Program.cs
@@ -1,5 +1,21 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using System;
 
+Console.WriteLine("Quel est le prix hors taxes de l'article ?");
+string prix = Console.ReadLine();
+int A = int.Parse(prix);
+
+Console.WriteLine("Quel est le nombre d'articles ?");
+string nombre = Console.ReadLine();
+int B = int.Parse(nombre);
+
+Console.WriteLine("Quel est le taux de TVA ?");
+string taux = Console.ReadLine();
+int C = int.Parse(taux);
+
+int resultat = ((20 / 100) + 1) * A * B;
+
+Console.WriteLine($"Le prix total est de {resultat}");
+
 
 
