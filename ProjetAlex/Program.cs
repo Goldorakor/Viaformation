467191b3c5dc9a4daf9f930aa073a26a7c7e9235
MichaëlHEID <MichalHEID@TRD013-SBG2FR>
2023-09-26 13:20:02 +0200

    exercice 23 fini

--- ProjetAlex/Program.cs
+++ ProjetAlex/Program.cs
@@ -1,5 +1,11 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using System;
 
+Console.WriteLine("Salut! Quel est ton prénom ?");
+
+string prenom = Console.ReadLine();
+
+Console.WriteLine($"Salut {prenom}");
+
 
 
