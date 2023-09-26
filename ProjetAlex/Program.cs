607ff990e4ad2d0303e813523a95a57a2dc63848
MichaëlHEID <MichalHEID@TRD013-SBG2FR>
2023-09-26 13:40:57 +0200

    résolution exercice 22

--- ProjetAlex/Program.cs
+++ ProjetAlex/Program.cs
@@ -1,17 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-using System;
-
+﻿
 Console.WriteLine("Salut! Comment vas-tu ?");
-
 Console.WriteLine("Donne un nombre et je te calcule son carré :");
 
 string A = Console.ReadLine();
-int B = int.Parse(A);
 
+int B = int.Parse(A);
 int C = B * B;
 
-Console.WriteLine("La valeur du carré de ton nombre est ");
-Console.WriteLine(C);
-
-
+Console.WriteLine($"La valeur du carré de {B} est {C}");
 
