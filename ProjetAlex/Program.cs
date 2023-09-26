1f714ddee41bf7495ebd11b4ace633854b714ded
MichaëlHEID <MichalHEID@TRD013-SBG2FR>
2023-09-26 11:30:27 +0200

    exercice 18

--- ProjetAlex/Program.cs
+++ ProjetAlex/Program.cs
@@ -1,5 +1,13 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using System;
 
+Console.WriteLine("exercice 18 : la solution");
+string A;
+string B;
+string C;
+
+A = "423";
+B = "12";
+C = A + B;
 
 
