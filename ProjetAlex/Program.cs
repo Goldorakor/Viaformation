1122e76368da9329bb9280eceadd80f4f5aa4808
MichaëlHEID <MichalHEID@TRD013-SBG2FR>
2023-10-03 09:34:08 +0200

    exercice 31

--- ProjetAlex/Program.cs
+++ ProjetAlex/Program.cs
@@ -1,5 +1,30 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using System;
 
+Console.WriteLine("Ecrire un nombre de votre choix ?");
+string nbsaisi = Console.ReadLine();
+decimal nb = decimal.Parse(nbsaisi);
+
+if (nb < 0 )
+{
+    Console.WriteLine("Le nombre écrit est négatif");
+}
+
+else if (nb > 0)
+{
+    Console.WriteLine("Le nombre écrit est positif");
+}
+
+else if (nb == 0)
+{
+    Console.WriteLine("Le nombre écrit vaut zéro");
+}
+
+else
+{
+    Console.WriteLine("Je ne sais pas déterminer le signe de ce nombre!");
+}
+
+
 
 
