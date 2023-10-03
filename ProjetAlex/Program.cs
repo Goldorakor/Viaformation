06a02448611ed03221258d9b669cd6c35609b56b
MichaëlHEID <MichalHEID@TRD013-SBG2FR>
2023-10-03 11:44:30 +0200

    exercice 32 fini

--- ProjetAlex/Program.cs
+++ ProjetAlex/Program.cs
@@ -3,19 +3,23 @@ using System;
 // See https://aka.ms/new-console-template for more information
 using System;
 
+
+// on demande la saisie d'un premier nombre à l'utilisateur et on corrige éventuellement le point en virgule
 Console.WriteLine("Ecrire un nombre de votre choix ?");
 string nbsaisi01 = Console.ReadLine();
 nbsaisi01 = nbsaisi01.Replace('.', ',');
 
+
+// on demande la saisie d'un second nombre à l'utilisateur et on corrige éventuellement le point en virgule
 Console.WriteLine("Ecrire un autre nombre de votre choix ?");
 string nbsaisi02 = Console.ReadLine();
 nbsaisi02 = nbsaisi02.Replace('.', ',');
 
 
-// decimal nb = decimal.Parse(nbsaisi);
-
+// on transforme la saisie string en nombre décimal et on l'affecte à nb01 et nb02
 bool succes01 = decimal.TryParse(nbsaisi01, out decimal nb01);
 bool succes02 = decimal.TryParse(nbsaisi02, out decimal nb02);
+
 
 if (succes01 == true && succes02 == true)
 {
