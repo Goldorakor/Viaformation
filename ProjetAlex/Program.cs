528a8a3dd5dd12660df40ab4b80dbfeff3a104ea
MichaëlHEID <MichalHEID@TRD013-SBG2FR>
2023-10-10 11:32:58 +0200

    exo 41

--- ProjetAlex/Program.cs
+++ ProjetAlex/Program.cs
@@ -1,5 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-using System;
+﻿
+
+// on demande la saisie d'un premier nombre à l'utilisateur et on corrige éventuellement le point en virgule
+Console.WriteLine("Ecrire un nombre de votre choix qui est compris entre 1 et 3.");
+string nbsaisi01 = Console.ReadLine();
+nbsaisi01 = nbsaisi01.Replace('.', ',');
+
+
+
+// on transforme la saisie string en nombre décimal et on l'affecte à nb01
+bool succes01 = decimal.TryParse(nbsaisi01, out decimal nb01);
+
+if succes01 = true
+
+while (nb01 < 1 && nb01 > 3)
+{
+    nbsaisi01 = Console.ReadLine();
+}
+
+  
+
+else
+{
+    Console.WriteLine("Bravo, votre nombre est bien compris entre 1 et 3");
+}
 
 
 
