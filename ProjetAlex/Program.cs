33ac251c5f52e2f11cada3f340577ae4e8fafe27
MichaëlHEID <MichalHEID@TRD013-SBG2FR>
2023-10-10 11:54:44 +0200

    exo 41 fini

--- ProjetAlex/Program.cs
+++ ProjetAlex/Program.cs
@@ -5,24 +5,22 @@ Console.WriteLine("Ecrire un nombre de votre choix qui est compris entre 1 et 3.
 string nbsaisi01 = Console.ReadLine();
 nbsaisi01 = nbsaisi01.Replace('.', ',');
 
+int nb01 = int.Parse(nbsaisi01);
 
+bool saisievalide = nb01 >= 1 && nb01 <= 3;
 
-// on transforme la saisie string en nombre décimal et on l'affecte à nb01
-bool succes01 = decimal.TryParse(nbsaisi01, out decimal nb01);
+// controle de la saisie
 
-if succes01 = true
-
-while (nb01 < 1 && nb01 > 3)
+while (saisievalide == false)
 {
+    Console.WriteLine("ce nombre n'est pas compris entre 1 et 3.");
+    Console.WriteLine("Ecrire un nombre de votre choix qui est compris entre 1 et 3.");
     nbsaisi01 = Console.ReadLine();
-}
-
-  
-
-else
-{
-    Console.WriteLine("Bravo, votre nombre est bien compris entre 1 et 3");
+    nb01 = int.Parse(nbsaisi01);
+    saisievalide = nb01 >= 1 && nb01 <= 3;
 }
 
 
+// message si réponse ok
+Console.WriteLine("Bravo, le nombre saisi est compris entre 1 et 3.");
 
