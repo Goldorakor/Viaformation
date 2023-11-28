0cebdeebdc930fe16a6515c5a3c2488c5acdef4b
MichaëlHEID <MichalHEID@TRD013-SBG2FR>
2023-11-28 11:35:07 +0100

    tableau

--- ProjetAlex/Program.cs
+++ ProjetAlex/Program.cs
@@ -1,5 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿// Faire saisir un tableau à l'utilisateur
+
+
 using System;
+
+decimal[] elements = new decimal[7];
+
+
+for (int i = 0; i < 7; i++)
+{
+    elements[i] = i;
+}
+
+for (int i = 0; i < 7; i++)
+{
+    console.writeline($"elements[i]");
+}
+
+
 
 
 
