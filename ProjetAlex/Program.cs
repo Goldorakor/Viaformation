9d76bf70b447020476c32c08d128de1bb4056df3
MichaëlHEID <MichalHEID@TRD013-SBG2FR>
2023-11-28 10:32:46 +0100

    provisoire

--- ProjetAlex/Program.cs
+++ ProjetAlex/Program.cs
@@ -1,5 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-using System;
+﻿// on demande la saisie à l'utilisateur d'un nombre entre 10 et 20
+Console.WriteLine("Ecrire un nombre entre 10 et 20, s'il te plait ?");
+string nbsaisi01 = Console.ReadLine();
+
+// on transforme la saisie string en nombre entier et on l'affecte à age
+bool succes01 = int.TryParse(nbsaisi01, out int nombre);
+
+
+
+if (succes01)
+{
+    while (nombre < 10)
+      
+        {
+            Console.WriteLine("Plus grand !");
+        Console.WriteLine("Ecrire un nombre entre 10 et 20, s'il te plait ?");
+        string nbsaisi01 = Console.ReadLine();
+       
+    }
+        
+
+   
+
+    Console.WriteLine("Bravo !");
+}
+
+else
+{
+    Console.WriteLine("Merci de rentrer un nombre !");
+}
 
 
 
