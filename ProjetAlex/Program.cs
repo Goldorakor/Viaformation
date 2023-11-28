465072a8bd4cee73179911add6263e527fd72d09
MichaëlHEID <MichalHEID@TRD013-SBG2FR>
2023-11-28 13:47:04 +0100

    essai 07

--- ProjetAlex/Program.cs
+++ ProjetAlex/Program.cs
@@ -1,15 +1,16 @@
 ﻿// on demande la saisie d'un nombre à l'utilisateur
-using System;
 
 Console.WriteLine("Merci de m'indiquer un nombre pour définir la taille du tableau.");
 string nbsaisi01 = Console.ReadLine();
 
 // on transforme la saisie string en nombre entier et on l'affecte à nb01
+
 bool succes01 = int.TryParse(nbsaisi01, out int nb01);
 
 
 
 // Solution plus évolutive avec boucle for :
+
 if (succes01 == true)
 {
     int[] tableau2 = new int[nb01];
